fbdafb3dd89cb91a2eea575cbd6520c09072a7c0
ValeriaGustin <valeriafgm64@gmail.com>
2025-02-13 23:03:25 +0000

    NEW: monte carlo no hilos con tiempo

--- montepi.cpp
+++ montepi.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstdlib>
 #include <cstdio>
 #include <random>
 #include <cmath>
+#include <ctime>
+
 using namespace std;
 
 random_device rd;
@@ -26,11 +27,29 @@ float idk(int tosses){
 	return ans;
 }
 
+void finish(clock_t startCpu, time_t startWall){
+    
+	clock_t endCpu = clock();
+    	time_t endWall = time(nullptr);
+
+        double cpuTime = double(endCpu - startCpu) / CLOCKS_PER_SEC;
+	double wallTime = difftime(endWall, startWall);
+	
+	printf("Tiempo de CPU (segundos): %.6f\n", cpuTime);
+	printf("Tiempo real (segundos): %.6f\n", wallTime);
+}
+
+
 int main(int argc, char* argv[]){
+
+        clock_t startCpu = clock();
+        time_t startWall = time(nullptr);
 
         int tosses = atoi(argv[1]);
 	float ans;
         ans = idk(tosses);
         printf("Result: %.6f\n", ans);
+
+	finish(startCpu, startWall);
         return 0;
 }
